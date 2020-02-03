89dab7d123a84f75a38eed8fd572481e401b7d41
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-02-03 22:06:03 +0200

    Added ProductNameValidationRule

--- src/com/javaguru/shoppinglist/service/validation/ProductValidationRule.java
+++ src/com/javaguru/shoppinglist/service/validation/ProductValidationRule.java
@@ -3,7 +3,7 @@ package com.javaguru.shoppinglist.service.validation;
 import com.javaguru.shoppinglist.domain.Product;
 
 public interface ProductValidationRule {
-    void validate(Product product);
+    void validate(Product product) throws ProductValidationException;
 
     default void checkNotNull(Product product) throws ProductValidationException {
         if (product == null) {
