490778d75874b7f77ef156c793e3a0a35bea34b7
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-02-08 23:39:24 +0200

    Added initMock. Still doesn't work

--- src/test/java/com/javaguru/shoppinglist/service/ProductServiceTest.java
+++ src/test/java/com/javaguru/shoppinglist/service/ProductServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
@@ -17,23 +18,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 class ProductServiceTest {
     Product testProduct;
 
     @Mock
-    ProductInMemoryRepository repository = mock(ProductInMemoryRepository.class);
+    ProductInMemoryRepository repository;
 
     @Mock
-    ProductValidationService validationService = mock(ProductValidationService.class);
+    ProductValidationService validationService;
 
     @InjectMocks
-    ProductService productService = new ProductService();
+    ProductService productService;
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+
         testProduct = new Product();
         testProduct.setPrice(new BigDecimal(50));
         testProduct.setDiscount(new BigDecimal(10));
@@ -45,8 +50,8 @@ class ProductServiceTest {
     void createProduct() {
         when(repository.insert(testProduct)).thenReturn(testProduct);
         productService.createProduct(testProduct);
-        verify(validationService, times(1)).validate(testProduct);
-        verify(repository, times(1)).insert(testProduct);
+        verify(validationService).validate(testProduct);
+        verify(repository).insert(testProduct);
     }
 
     @Test
