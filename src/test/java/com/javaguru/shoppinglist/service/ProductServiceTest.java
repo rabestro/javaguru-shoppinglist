5e90ebb504d4a3f6d00a8698632a257618e369c2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-02-08 22:39:55 +0200

    Added Mock. But it doesn't work...

---
+++ src/test/java/com/javaguru/shoppinglist/service/ProductServiceTest.java
@@ -0,0 +1,71 @@
+package com.javaguru.shoppinglist.service;
+
+import com.javaguru.shoppinglist.domain.Product;
+import com.javaguru.shoppinglist.repository.ProductInMemoryRepository;
+import com.javaguru.shoppinglist.service.validation.ProductValidationService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+class ProductServiceTest {
+    Product testProduct;
+
+    @Mock
+    ProductInMemoryRepository repository = mock(ProductInMemoryRepository.class);
+
+    @Mock
+    ProductValidationService validationService = mock(ProductValidationService.class);
+
+    @InjectMocks
+    ProductService productService = new ProductService();
+
+    @BeforeEach
+    void setUp() {
+        testProduct = new Product();
+        testProduct.setPrice(new BigDecimal(50));
+        testProduct.setDiscount(new BigDecimal(10));
+        testProduct.setName("Apple");
+        testProduct.setDescription("Green apples");
+    }
+
+    @Test
+    void createProduct() {
+        when(repository.insert(testProduct)).thenReturn(testProduct);
+        productService.createProduct(testProduct);
+        verify(validationService, times(1)).validate(testProduct);
+        verify(repository, times(1)).insert(testProduct);
+    }
+
+    @Test
+    void findProductById() {
+        when(repository.findProductById(1l)).thenReturn(testProduct);
+        assertEquals(testProduct, productService.findProductById(1l));
+    }
+
+
+    @Test
+    public void whenNotUseMockAnnotation_thenCorrect() {
+        List mockList = Mockito.mock(ArrayList.class);
+
+        mockList.add("one");
+        Mockito.verify(mockList).add("one");
+        assertEquals(0, mockList.size());
+
+        Mockito.when(mockList.size()).thenReturn(100);
+        assertEquals(100, mockList.size());
+    }
+
+}
